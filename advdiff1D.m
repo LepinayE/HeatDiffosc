453c1fc777309dbaeb23d1ee23ba2b02bd2bc48e
LepinayE <98457289+LepinayE@users.noreply.github.com>
2022-03-17 15:50:43 +0000

    Add files via upload

---
+++ advdiff1D.m
@@ -0,0 +1,83 @@
+%{
+Solving the 1D advection-diffusion equation for a interseasonal injection
+extraction system 
+T_t + USin(wt) T_x = K T_xx (non dim)
+
+with boundary conditions:
+
+Injection temp kept constant: 
+T(0) = 1  for 0 < t < pi/w
+
+Purely advective flux at origin during outflow:
+T_x  = 0 at x=0 for pi/w < t< 2pi /w
+%}
+
+function [x,T,t] = advdiff1D(L,U) % Extract the first solution component as T. 
+
+% Mesh over which PDE is solved
+w = 1;
+L = 10;
+t =  linspace(0,pi/w,10); % time 0<t< pi/w
+res = 50;
+x = linspace(0,L,res);
+
+
+m = 0; %symmetry none as injection in LHS
+sol = pdepe(m,@pdex1pde,@pdex1ic,@bcfun,x,t); % PDE solver
+%{
+ pdex1pde = PDE equation
+ pdex1ic = initial conditions
+ bcfun = boundary conditions 
+ %}
+
+T = sol(:,:,1);
+
+
+figure (1)
+hold on
+plot(x,T,'k-')
+xlabel ('x')
+ylabel('Temp')
+
+
+figure (2)
+hold on 
+plot(t,T)
+xlabel ('time')
+ylabel('Temp')
+
+
+
+
+    % PDE
+    function [c,f,s] = pdex1pde(x,t,T,dTdx)
+    
+    k = 10; %k needs to be found
+    U = 1; % velocity u might not be constant
+        
+    c = 1;
+    f = (k*dTdx);
+    s = -U*sin(w*t)*dTdx; 
+    end
+
+    function T0 = pdex1ic(x) %initial condition
+   
+    if x>0
+    T0 = 0.0001;
+    else
+        T0=1;
+    end
+    end
+
+    function [pL,qL,pR,qR] = bcfun(xL,TL,xR,TR,t)
+    % BCs at _L left boundary (injection) and _R right boundary (far field)
+    % BCs take form p(x,t,T) + q(x,t)f(x,t,T,dTdx)= 0
+   
+    pL =TL-1; 
+    qL = 0;
+    pR = TR-0.0001;
+    qR = 0;
+    end
+
+
+end
